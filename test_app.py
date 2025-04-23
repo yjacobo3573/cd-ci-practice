857eaf9abd9bae121076ad00c8c349dac8e65361
Yulisa Sandoval-Jacobo <90855493+yjacobo3573@users.noreply.github.com>
2025-04-22 19:49:28 -0500

    Create test_app.py

---
+++ test_app.py
@@ -0,0 +1,8 @@
+from app import app
+
+
+def test_home():
+    with app.test_client() as client:
+        response = client.get("/")
+        assert response.status_code == 200
+        assert b"Hello" in response.data
